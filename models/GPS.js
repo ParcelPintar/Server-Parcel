e93e95387e2a42b2f4dbd17d12546fa1bbb15052
kneric <brian.pradipta.a@gmail.com>
2018-09-19 05:26:10 +0700

    edit GPS

--- models/GPS.js
+++ models/GPS.js
@@ -3,8 +3,15 @@ const Schema = mongoose.Schema;
 
 const GPSSchema = Schema({
   location: {
-    type: { type: String },
-    coordinates: []
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ['Point'], // 'location.type' must be 'Point'
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
   }
 })
 
