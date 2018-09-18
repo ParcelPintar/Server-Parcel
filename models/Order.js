00e1c788227b7008cfcde02d0f4ac40655a7c94c
kneric <brian.pradipta.a@gmail.com>
2018-09-19 05:28:28 +0700

    added order model

---
+++ models/Order.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orderSchema = Schema({
+  status: String,
+  sender: {
+    type: Schema.Types.ObjectId, ref: 'User',
+    required: true
+  },
+  receiver: { 
+    type: Schema.Types.ObjectId, ref: 'User',
+    required: true
+  },
+  address: { 
+    type: String,
+    required: true
+  },
+  destination: {
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ['Point'] // 'location.type' must be 'Point'
+    },
+    coordinates: {
+      type: [Number]
+    }
+  }
+})
+
+module.exports = mongoose.model('ParcelPintar', orderSchema)
