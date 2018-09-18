f6b52ed1d7a6027285f625f237297cf6a1cf85e3
kneric <brian.pradipta.a@gmail.com>
2018-09-19 05:30:46 +0700

    change required key for pp model

--- models/ParcelPintar.js
+++ models/ParcelPintar.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const parcelPintarSchema = Schema({
-  gyro: { type: Schema.Types.ObjectId, ref: 'Gyro' },
-  gps: { type: Schema.Types.ObjectId, ref: 'GPS' }
+  gyro: { 
+    type: Schema.Types.ObjectId, ref: 'Gyro',
+    required: true
+  },
+  gps: { 
+    type: Schema.Types.ObjectId, ref: 'GPS',
+    required: true 
+  }
 })
 
 module.exports = mongoose.model('ParcelPintar', parcelPintarSchema)
