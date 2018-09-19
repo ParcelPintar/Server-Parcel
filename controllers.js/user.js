0096736df9965c1dbed122aaedf8b23a46f75adc
kneric <brian.pradipta.a@gmail.com>
2018-09-19 11:22:40 +0700

    CRUD user

--- controllers.js/user.js
+++ controllers.js/user.js
@@ -4,7 +4,7 @@ const AuthHelper = require('../helpers/authHelper');
 class UserController {
   constructor() {}
 
-  static signup (req, res) {
+  static register (req, res) {
     const {
       name,
       email,
@@ -47,6 +47,56 @@ class UserController {
         } else {
           res.status(404).json({
             error: 'User not registered'
+          })
+        }
+      })
+      .catch(err => {
+        res.status(400).json({
+          error: err.message
+        })
+      })
+  }
+
+  static update (req, res) {
+    let userId = req.params.id;
+
+    User.findByIdAndUpdate(userId, {$set: req.body}, {new:true})
+      .then( updatedUser => {
+        res.status(200).json(updatedUser)
+      })
+      .catch( err => {
+        res.status(400).json({
+          error: err.message
+        })
+      })
+  }
+
+  static remove (req, res) {
+    let userId = req.params.id;
+
+    User.findByIdAndRemove(userId)
+      .then( removedUser => {
+        res.status(200).json(removedUser)
+      })
+      .catch( err => {
+        res.status(400).json({
+          error: err.message
+        })
+      })
+  }
+
+  static getUser (req, res) {
+    const {email} = req.body
+
+    User.findOne (email)
+      .populate('orders')
+      .populate('receivers')
+      .then(userFound => {
+        if(userFound){
+          res.status(200).json(userFound)
+        } else {
+          res.status(404).json({
+            error: 'User not found'
           })
         }
       })
