9d7f5cf08435f7614187d23cf31cc9855308c036
Joan Lamrack <joanlamrack@gmail.com>
2018-09-22 22:02:16 +0700

    mocha automatic exit

--- test/authhelper_test.js
+++ test/authhelper_test.js
@@ -2,13 +2,12 @@ const chai = require("chai");
 let expect = chai.expect;
 const authHelper = require("../helpers/authHelper");
 
-
-describe("Auth Helper", function(){
-	it("should match password and hash correctly", done=>{
+describe("Auth Helper", function() {
+	it("should match password and hash correctly", done => {
 		let password = "askjdfjasjdfas";
 		let hashedPassword = authHelper.hashpass(password);
-		let matchWithHash = authHelper.comparehash(password,hashedPassword);
+		let matchWithHash = authHelper.comparehash(password, hashedPassword);
 		expect(matchWithHash).to.be.equal(true);
-		done()
+		done();
 	});
-})
+});
